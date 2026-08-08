def72ba3f7ddb450815eea3a3554a7317f529738
Yuvraj <94313070+ertyukmnbd@users.noreply.github.com>
2026-08-09 01:05:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (12.43%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double Fah = celsius*1.80 + 32.00;
+     return {kelvin,Fah};    }
+};
